bd10e6f58ed120edcb75cf5f51079aacaba639a4
Blancaclaire <clara.alonso.jimenez@gmail.com>
2025-06-18 15:18:54 +0200

    seleccion de inputs en js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
 changeContent();
 
-console.log('pasa algo o no?')
 });
 
 
@@ -9,13 +8,38 @@ function changeContent() {
     const preview = document.getElementById('empty-result');
     const realResult = document.getElementById('real-result');
     const submitButtomn= document.getElementById('subB');
+    const form =document.getElementById('mortgage-form');
 
     submitButtomn.addEventListener('click', function(event){
         event.preventDefault();
-        console.log('el boton sabe que algo pasa')
         preview.classList.remove('active');
         realResult.classList.add('active');
+
+        calculator();
+
+
+        // form.submit();
     });
 
+}
+
+function calculator(){
+
+const MortgageAmount = document.getElementById('mortgageAmount').value;
+console.log('La cantidad es ' + MortgageAmount);
+
+const MortgageTerm= document.getElementById('mortgageTerm').value;
+console.log('La cantidad es ' + MortgageTerm);
+
+const InterestRate = document.getElementById('mortgageInterest').value;
+console.log('La cantidad es ' + InterestRate);
+
+
+const InputType = document.querySelector('.mortgagetype').value;
+console.log('la cantidad es ' + InputType);
+
+if(!MortgageAmount){
+    alert('falta algo');
+}
 
 }
