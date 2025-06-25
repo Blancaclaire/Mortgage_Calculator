dd1d6856af31baa4590ed3c222acdbc879b4a4e6
Blancaclaire <clara.alonso.jimenez@gmail.com>
2025-06-25 13:26:07 +0200

    funcion de control para inputs vacios y cambios en la estructura de la tarjeta para ajutarse en laptops sin hacer scroll

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
-changeContent();
+document.addEventListener('DOMContentLoaded', function () {
+    changeContent();
 
 });
 
@@ -7,39 +7,64 @@ changeContent();
 function changeContent() {
     const preview = document.getElementById('empty-result');
     const realResult = document.getElementById('real-result');
-    const submitButtomn= document.getElementById('subB');
-    const form =document.getElementById('mortgage-form');
+    const submitButtomn = document.getElementById('subB');
+    const form = document.getElementById('mortgage-form');
 
-    submitButtomn.addEventListener('click', function(event){
+    submitButtomn.addEventListener('click', function (event) {
         event.preventDefault();
         preview.classList.remove('active');
         realResult.classList.add('active');
 
         calculator();
-
+        visibilityControl();
 
         // form.submit();
     });
 
 }
 
-function calculator(){
-
-const MortgageAmount = document.getElementById('mortgageAmount').value;
-console.log('La cantidad es ' + MortgageAmount);
-
-const MortgageTerm= document.getElementById('mortgageTerm').value;
-console.log('La cantidad es ' + MortgageTerm);
-
-const InterestRate = document.getElementById('mortgageInterest').value;
-console.log('La cantidad es ' + InterestRate);
+function calculator() {
 
 
-const InputType = document.querySelector('.mortgagetype').value;
-console.log('la cantidad es ' + InputType);
+    const MortgageAmount = document.getElementById('mortgageAmount').value;
+    console.log('La cantidad es ' + MortgageAmount);
 
-if(!MortgageAmount){
-    alert('falta algo');
+    const MortgageTerm = document.getElementById('mortgageTerm').value;
+    console.log('La cantidad es ' + MortgageTerm);
+
+    try {
+        const InterestRate = document.getElementById('mortgageInterest').value;
+        console.log('La cantidad es ' + InterestRate);
+    }
+
+    catch {
+        console.log(errorM)
+    }
 }
 
+
+function visibilityControl() {
+
+
+    const wrappers = document.querySelectorAll('.input-wrapper');
+
+    wrappers.forEach(wrapper => {
+        const input = wrapper.querySelector('input');
+        const symbol = wrapper.querySelector('.input-symbol');
+        const errorM = wrapper.querySelector('.errorMessaje');
+
+        if (input.value.trim() === "") {
+            symbol.classList.add('empty');
+            input.classList.add('empty');
+            errorM.classList.add('active');
+
+        }
+    });
+    
+    const InputType = document.querySelector('.mortgagetype:checked');
+    if(!InputType){
+        console.log('que pasooooo')
+    }
+
 }
+
